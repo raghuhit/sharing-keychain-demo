2ff2854244f377286bd26af50754d970d3d81f8f
Hitesh Raghuvanshi <hitesh@browserstack.com>
2019-06-27 16:35:11 +0530

    Adding custom changes to actual app

--- OneKeychain/ViewController.swift
+++ OneKeychain/ViewController.swift
@@ -14,20 +14,29 @@ class ViewController: UIViewController {
   
   let keychainWrapper = KeychainTextWrapper()
   let keychainItemKey = "myKey"
+  var counter = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let appIdPrefix = Bundle.main.object(forInfoDictionaryKey: "AppIdentifierPrefix")
-    print(appIdPrefix)
-    
-    // Use your App ID Prefix here instead of 'AB123CDE45'
-    // To find out what it is: http://evgenii.com/blog/sharing-keychain-in-ios/
-    keychainWrapper.accessGroup = "AB123CDE45.myKeychainGroup1"
+    // Get app id prefix for accessing keychain from Info.plist
+    let appIdPrefix = Bundle.main.object(forInfoDictionaryKey: "AppIdentifierPrefix") as! String
+    // Uncomment following if hardcoding app id prefix in app.
+//    let appIdPrefix = "8XPT93F9Q7"
+    print("AppIdPrefix : "+appIdPrefix)
+
+    // keychainAccessIdentifier is the identifier mentioned in entitlements file for keychain-access-groups
+    // Uncomment following line if your keychain access group identifier is same as bundleid and comment the next line where we are hardcoding it
+//  let keychainAccessIdentifier = Bundle.main.bundleIdentifier as! String
+    let keychainAccessIdentifier = "myKeychainGroup1"
+    print("keychainAccessIdentifier : "+keychainAccessIdentifier)
+//    "8XPT93F9Q7.com.sample.example"
+    keychainWrapper.accessGroup = appIdPrefix + keychainAccessIdentifier
   }
 
   @IBAction func didTapAddButton(_ sender: AnyObject) {
-    keychainWrapper.add(keychainItemKey, itemValue: "myValue")
+    counter += 1
+    keychainWrapper.add(keychainItemKey, itemValue: "myValue \(counter)")
     showKeychainItem()
   }
   
